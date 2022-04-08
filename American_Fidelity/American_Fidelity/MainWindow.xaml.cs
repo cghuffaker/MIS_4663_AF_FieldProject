2941e748fd3391d366e5b777f6c00d2937039328
StanleySch <stanley.s.schmidt-1@ou.edu>
2022-04-08 16:47:54 -0500

    read in the software csv file and put the data in the software listbox

--- American_Fidelity/American_Fidelity/MainWindow.xaml.cs
+++ American_Fidelity/American_Fidelity/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,11 +24,18 @@ namespace American_Fidelity
         public MainWindow()
         {
             InitializeComponent();
+            string[] linesOfFile = File.ReadAllLines("ARIS_SoftwareCatalog_OUProject.csv");
 
-        }
+            for (int i = 1; i < linesOfFile.Length; i++)
+            {
+                Software entries = new Software(linesOfFile[i], i);
 
-        private void lst_Attributes_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
+                lst_Softwares.Items.Add(entries.Name);
+            }
+
+
+        //private void lst_Attributes_SelectionChanged(object sender, SelectionChangedEventArgs e)
+       // {
 
         }
     }
---
+++ American_Fidelity/American_Fidelity/Software.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace American_Fidelity
+{
+    public class Software
+    {
+        public string Name { get; set; }
+        public string Manufacturer_Name { get; set; }
+
+        public string? Product_Family_Name { get; set; }
+
+        public string Product_Name { get; set; }
+        public string Software_Acquisition_Method { get; set; }
+        public string GUID { get; set; }
+        public string? Description { get; set; }
+        public string? Link_1 { get; set; }
+
+        public  Software()
+        {
+            Name = string.Empty;
+
+            Manufacturer_Name = string.Empty;
+
+            Product_Family_Name = string.Empty;
+
+            Product_Name = string.Empty;
+
+            Software_Acquisition_Method = string.Empty;
+
+            GUID = string.Empty;
+
+            Description = string.Empty;
+                
+            Link_1 = string.Empty;
+
+        }
+
+        public Software(string info, int LineNum = 0)
+        {
+            string[] pieces = info.Split(',');
+            Name = pieces[0];
+            Manufacturer_Name = pieces[1];
+            Product_Family_Name = pieces[2];
+            Product_Name = pieces[3];
+            Software_Acquisition_Method = pieces[4];
+            GUID = pieces[5];
+            Description = pieces[6];
+            Link_1 = pieces[7];
+
+        }
+    }
+}
